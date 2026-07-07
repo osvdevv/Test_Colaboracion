ec37e3803211f20f32d6aa3f1610b1ee67464235
Rufino Velázquez Luis Osvaldo <250110286@itsoeh.edu.mx>
2026-07-06 22:08:17 -0600

    Creacion de funciones de el archivo asistencias y testeo pasado de todas

--- src/.ipynb_checkpoints/asistencias-checkpoint.py
+++ src/.ipynb_checkpoints/asistencias-checkpoint.py
@@ -16,20 +16,41 @@ def contar_asistencias(asistencias_alumno):
 
 
 def contar_faltas(asistencias_alumno):
-    pass
+    return asistencias_alumno.count(0)
 
 
 def porcentaje_asistencia(asistencias_alumno):
-    pass
+    if not asistencias_alumno:
+        return 0.0
+
+    total_clases = contar_clases(asistencias_alumno)
+    asistencias = contar_asistencias(asistencias_alumno)
+
+    return (asistencias / total_clases) * 100
 
 
 def esta_en_riesgo(asistencias_alumno):
-    pass
+    return porcentaje_asistencia(asistencias_alumno) < 80
 
 
 def alumnos_en_riesgo(asistencias):
-    pass
+    en_riesgo = []
+
+    for alumno, lista_asistencias in asistencias.items():
+        if esta_en_riesgo(lista_asistencias):
+            en_riesgo.append(alumno)
+
+    return en_riesgo 
+            
 
 
 def promedio_asistencia_grupo(asistencias):
-    pass
+    if not asistencias:
+        return 0.0
+
+    suma_porcentajes = 0
+
+    for lista_asistencias in asistencias.values():
+        suma_porcentajes += porcentaje_asistencia(lista_asistencias)
+
+    return suma_porcentajes / len(asistencias)
