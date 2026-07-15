b28556f9bfb471b4707ca4ab2243d8942d76cc8e
Rufino Velázquez Luis Osvaldo <250110286@itsoeh.edu.mx>
2026-07-15 13:27:13 -0600

    Actualizacion

--- src/reportes.py
+++ src/reportes.py
@@ -9,11 +9,8 @@ from src.asistencias import promedio_asistencia_grupo
 from src.encuestas import respuesta_mas_comun
 
 def reporte_general(calificaciones, asistencias, respuestas):
-    """
-    Integracion de resultados de calificaciones, asistencias y encuestas en un mismo diccionario
-    """
     return {
-        "promedio_clasificaciones": promedio(calificaciones),
+        "promedio_calificaciones": promedio(calificaciones),
         "porcentaje_aprobados": porcentaje_aprobados(calificaciones),
         "promedio_asistencia": promedio_asistencia_grupo(asistencias),
         "respuesta_mas_comun": respuesta_mas_comun(respuestas)
